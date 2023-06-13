56e68a0bc7aeb4527fb2476f8c0900d7ad15bf99
Zachary <stallings.zach@icloud.com>
2023-06-13 09:05:40 -0700

    first

---
+++ backend/db/seeders/20230613155116-group-image-data.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+const { query } = require("express");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const groupImagesData = [
+      {
+        groupId: 1,
+        url: "/pic",
+        preview: true,
+      },
+      {
+        groupId: 2,
+        url: "/pic",
+        preview: true,
+      },
+      {
+        groupId: 3,
+        url: "/pic",
+        preview: true,
+      },
+    ];
+    options.tableName = "GroupImages";
+
+    return queryInterface.bulkInsert(options, groupImagesData, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+
+    options.tableName = "GroupImages";
+
+    await queryInterface.bulkDelete(
+      options,
+      {
+        groupId: { [Op.in]: [1, 2, 3] },
+      },
+      {}
+    );
+  },
+};
