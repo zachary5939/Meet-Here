e0a4568c5c0848bea1ba04c0620307acb299091f
Zachary <stallings.zach@icloud.com>
2023-06-29 15:19:49 -0700

    phase 2 done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,6 +4,34 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
     const data = await response.json();
@@ -11,32 +39,22 @@ export const restoreUser = () => async (dispatch) => {
     return response;
   };
 
-const setUser = (user) => {
-  return {
-    type: SET_USER,
-    payload: user,
+  export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
   };
-};
-
-const removeUser = () => {
-  return {
-    type: REMOVE_USER,
-  };
-};
-
-export const login = (user) => async (dispatch) => {
-  const { credential, password } = user;
-  const response = await csrfFetch("/api/session", {
-    method: "POST",
-    body: JSON.stringify({
-      credential,
-      password,
-    }),
-  });
-  const data = await response.json();
-  dispatch(setUser(data.user));
-  return response;
-};
 
 const initialState = { user: null };
 
@@ -55,5 +73,7 @@ const sessionReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+
 
 export default sessionReducer;
